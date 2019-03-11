ea2929944628766df445ba22acf339cee3118e29
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-03-11 16:49:10 +0800

    LeetCode solution203 Remove all elements from a linked list of integers that have value val.

---
+++ src/com/wcs/linkedlist/Solution203.java
@@ -0,0 +1,81 @@
+package com.wcs.linkedlist;
+
+/**
+ * @Description: Remove all elements from a linked list of integers that have value val.
+ * Example:
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
+ * @Author: WenChangSheng
+ * @Date: Created in 2019/3/11 16:32
+ */
+public class Solution203 {
+
+    /**
+     * 解决方式一
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements(ListNode head, int val) {
+        //当头部节点相同时
+        while (head != null && head.val == val) {
+            ListNode delNode = head;
+            head = head.next;
+            delNode.next = null;
+        }
+
+        if (head == null) {
+            return null;
+        }
+
+        //非头部节点
+        ListNode prev = head;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = head.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return head;
+    }
+
+    /**
+     * 解决方式二
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public ListNode removeElements2(ListNode head, int val) {
+        ListNode dummyHead = new ListNode(-1);
+        dummyHead.next = head;
+
+        ListNode prev = dummyHead;
+        while (prev.next != null) {
+            if (prev.next.val == val) {
+                ListNode delNode = head.next;
+                prev.next = delNode.next;
+                delNode.next = null;
+            } else {
+                prev = prev.next;
+            }
+        }
+
+        return dummyHead.next;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+}
