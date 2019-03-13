e7a862b6feb2e9930f3bdfe8b7415b8c5ff1ed91
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-03-13 11:51:26 +0800

    :sparkles:add Map

---
+++ src/com/wcs/map/MapTest.java
@@ -0,0 +1,54 @@
+package com.wcs.map;
+
+import com.wcs.set.FileOperation;
+
+import java.util.ArrayList;
+
+/**
+ * 时间复杂度与Set中分别用链表和二分搜索树实现的一样
+ */
+public class MapTest {
+
+    private static double testMap(Map<String, Integer> map, String filename) {
+
+        long startTime = System.nanoTime();
+
+        System.out.println(filename);
+        ArrayList<String> words = new ArrayList<>();
+        if (FileOperation.readFile(filename, words)) {
+            System.out.println("Total words: " + words.size());
+
+            for (String word : words) {
+                if (map.contains(word)) {
+                    map.set(word, map.get(word) + 1);
+                } else {
+                    map.add(word, 1);
+                }
+            }
+
+            System.out.println("Total different words: " + map.getSize());
+            System.out.println("Frequency of PRIDE: " + map.get("pride"));
+            System.out.println("Frequency of PREJUDICE: " + map.get("prejudice"));
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
+    }
+
+    public static void main(String[] args) {
+
+        String filename = "pride-and-prejudice.txt";
+
+        BSTMap<String, Integer> bstMap = new BSTMap<>();
+        double time1 = testMap(bstMap, filename);
+        System.out.println("BST Map: " + time1 + " s");
+
+        System.out.println();
+
+        LinkedListMap<String, Integer> linkedListMap = new LinkedListMap<>();
+        double time2 = testMap(linkedListMap, filename);
+        System.out.println("Linked List Map: " + time2 + " s");
+
+    }
+}
